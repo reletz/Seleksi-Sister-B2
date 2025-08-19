57edc1acacb6b0058d8bce126565954797ac7308
unknown <naufarrelzhafifabhista@gmail.com>
2025-08-19 10:46:44 +0700

    feat: added post

--- 1. Ngalegaan Kakawasan, Tamamo Silang - Sunda Priangan/server.asm
+++ 1. Ngalegaan Kakawasan, Tamamo Silang - Sunda Priangan/server.asm
@@ -13,6 +13,9 @@ entry start
 AF_INET     equ 2
 SOCK_STREAM equ 1
 O_RDONLY    equ 0
+O_WRONLY    equ 1
+O_CREAT     equ 64
+O_TRUNC     equ 512
 
 SYS_SOCKET  equ 41
 SYS_BIND    equ 49
@@ -36,17 +39,15 @@ segment readable writeable
   file_test db 'test.html', 0
 
   ; Header HTTP
-  http_200_get  db 'HTTP/1.1 200 OK', 0Dh, 0Ah, 'Content-Type: text/html', 0Dh, 0Ah, 0Dh, 0Ah
-  len_200       = $ - http_200_get
+  http_200      db 'HTTP/1.1 200 OK', 0Dh, 0Ah, 'Content-Type: text/html', 0Dh, 0Ah, 0Dh, 0Ah
+  len_200      = $ - http_200
 
-  http_200_post db 'HTTP/1.1 200 OK', 0Dh, 0Ah, 0Dh, 0Ah, 'POST request received.'
-  len_200_post  = $ - http_200_post
+  post_filename db 'post_result.txt', 0
+  http_201      db 'HTTP/1.1 201 Created', 0Dh, 0Ah, 0Dh, 0Ah, 'File created successfully.'
+  len_201      = $ - http_201
 
-  http_200_put  db 'HTTP/1.1 200 OK', 0Dh, 0Ah, 0Dh, 0Ah, 'PUT request received.'
-  len_200_put   = $ - http_200_put
-
-  http_200_del  db 'HTTP/1.1 200 OK', 0Dh, 0Ah, 0Dh, 0Ah, 'DELETE request received.'
-  len_200_del   = $ - http_200_del
+  http_400      db 'HTTP/1.1 400 Bad Request', 0Dh, 0Ah, 0Dh, 0Ah, '<h1>400 Bad Request</h1>'
+  len_400       = $ - http_400
 
   http_404      db 'HTTP/1.1 404 Not Found', 0Dh, 0Ah, 0Dh, 0Ah, '<h1>404 Not Found</h1>'
   len_404       = $ - http_404
@@ -96,6 +97,7 @@ accept_loop:
   syscall
   cmp rax, 0
   je  child_process
+
 parent_process:
   mov rax, SYS_CLOSE
   mov rdi, r13
@@ -112,47 +114,66 @@ child_process:
   mov rdx, 2048
   syscall
 
-  lea rdi, [request_buffer]
-  lea rsi, [method_post]
-  mov rcx, 5
-  repe cmpsb
+  mov r15, rax
+  mov eax, dword [request_buffer]
+
+  cmp eax, 'POST'
   je handle_post
 
-  lea rdi, [request_buffer]
-  lea rsi, [method_put]
-  mov rcx, 4  
-  repe cmpsb
+  cmp eax, 'PUT '
   je handle_put
 
-  lea rdi, [request_buffer] ;
-  lea rsi, [method_del]
-  mov rcx, 7
-  repe cmpsb
+  cmp eax, 'DELE'
   je handle_del
 
-  lea rdi, [request_buffer]
-  lea rsi, [method_get]
-  mov rcx, 4
-  repe cmpsb
-  je handle_get;
+  cmp eax, 'GET '
+  je handle_get
 
-  ; Jika tidak ada metode yang cocok, kirim 405 Method Not Allowed
+  ; Jika tidak ada yang cocok
   jmp handle_405
-  
+
 handle_post:
-  mov rsi, http_200_post
-  mov rdx, len_200_post
-  jmp send_response
+  xor rcx, rcx ; Gunakan rcx sebagai counter/index loop
+find_body_loop:
+  ; Bandingkan 4 byte dari posisi saat ini dengan 0A0D0A0D (CRLFCRLF)
+  mov eax, dword [request_buffer + rcx]
+  cmp eax, 0A0D0A0Dh
+  je found_body
+
+  inc rcx ; Pindah ke byte selanjutnya
+  cmp rcx, r15 ; Cek apakah sudah sampai akhir buffer
+  jl find_body_loop
+
+  ; Jika loop selesai tanpa menemukan body, anggap request tidak valid
+  jmp handle_400 ; (Kamu perlu buat handler ini)
+
+found_body:
+  ; Jika kita sampai sini, artinya kita menemukan separatornya.
+  ; Alamat awal body adalah: request_buffer + rcx + 4
+  lea rbx, [request_buffer + rcx + 4]
+
+  mov rdx, r15 ; r15 masih berisi total byte dari read
+  sub rdx, rcx
+  sub rdx, 4
+
+  mov r15, rdx
+
+  jmp write_post_to_file
 
 handle_put:
-  mov rsi, http_200_put
-  mov rdx, len_200_put
+  mov rsi, http_200
+  mov rdx, len_200
   jmp send_response
 
 handle_del:
-  mov rsi, http_200_del
-  mov rdx, len_200_del
+  mov rsi, http_200
+  mov rdx, len_200
   jmp send_response
+
+handle_400:
+  mov rsi, http_400      
+  mov rdx, len_400       
+  jmp send_response 
 
 handle_404:
   mov rsi, http_404       
@@ -164,19 +185,21 @@ handle_405:
   mov rdx, len_405
   jmp send_response
 
-handle_get: ; Routing
+handle_get:
+  ; --- Cek untuk path "/test" ---
   lea rdi, [request_buffer + 4]
   lea rsi, [path_test]
   mov rcx, 5
   repe cmpsb
-  jne check_root
+  jne check_root  ; Jika tidak diawali "/test", cek untuk "/"
 
   cmp byte [rdi], ' '
-  jne handle_404
+  je handle_test  ; Jika ya, ini path yang valid
 
-  jmp handle_test
+  jmp handle_404
 
 check_root:
+  ; --- Cek untuk path "/" ---
   mov al, byte [request_buffer + 4]
   mov ah, byte [request_buffer + 5]
   cmp al, '/'
@@ -192,6 +215,32 @@ handle_root:
 handle_test:
   mov rdi, file_test
   jmp serve_file
+
+write_post_to_file:
+  ; 1. Buka file untuk ditulis (O_WRONLY | O_CREAT | O_TRUNC)
+  mov rax, SYS_OPEN
+  mov rdi, post_filename
+  mov rsi, O_WRONLY or O_CREAT or O_TRUNC ; Flags: WRONLY | CREAT | TRUNC
+  mov rdx, 644o        ; Mode izin file
+  syscall
+  mov r14, rax ; Simpan file descriptor
+
+  ; 2. Tulis body ke file
+  mov rax, SYS_WRITE
+  mov rdi, r14 ; file descriptor
+  mov rsi, rbx ; pointer ke body (dari hasil pencarian)
+  mov rdx, r15 ; berisi panjang body (dari hasil pencarian)
+  syscall
+
+  ; 3. Tutup file
+  mov rax, SYS_CLOSE
+  mov rdi, r14
+  syscall
+
+  ; 4. Kirim respons ke klien
+  mov rsi, http_201
+  mov rdx, len_201
+  jmp send_response
 
 serve_file:
   mov rax, SYS_OPEN
@@ -212,7 +261,7 @@ serve_file:
   syscall
   mov rax, SYS_WRITE
   mov rdi, r13
-  mov rsi, http_200_get
+  mov rsi, http_200
   mov rdx, len_200
   syscall
   mov rax, SYS_WRITE
